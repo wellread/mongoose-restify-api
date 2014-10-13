8e8ed5a03249e14cfe015295ca5ec43960cb8737
Emmanuel Mwangi <emmanuelm@gmail.com>
2014-10-12 18:34:37 -0700

    Adding app server

---
+++ app/server.js
@@ -0,0 +1,52 @@
+// *******************
+// ** app/server.js **
+var restify =  require('restify');
+var mongoose = require('mongoose');
+
+// **************************
+// **  Bootstrapping ********
+
+// connect to mongodb database rest-api on localhost
+mongoose.connect('mongodb://localhost/rest-api')
+
+// create restify serer named 'rest-api'
+var server = restify.createServer({
+    name: 'rest-api',
+})
+
+// Use body parser middleware to parse json post body
+server.use(restify.bodyParser());
+
+// ******************
+// ** Routing *******
+
+// Restful url should be seperated in logical resources (noun) accessed
+// via HTTP methods
+
+// HTTP Methods
+
+/* GET /messages
+ * List all messages */
+server.get('/messages',     require('./routes/messages/list'));
+
+/* GET /messages/:id
+ * Get message details by id */
+server.get('/messages/:id', require('./routes/messages/details'));
+
+/* PUT /messages/:id
+ * Update messages by id */
+server.put('/messages/:id', require('./routes/messages/update'));
+
+/* DEL /messages/:id
+ * Delete messages by id */
+server.del('/messages/:id', require('./routes/messages/delete'));
+
+/* POST /messages
+ * Create new messages */
+server.post('/messages',    require('./routes/messages/create'));
+
+// ******************
+// ** Starting ******
+
+// start listening on port 1337
+server.listen(1337);
